6b9deb2e8111c491e87c3d4b7e685090d77a24d4
judykim <judykim@carrotglobal.com>
2020-02-20 17:21:39 +0900

    api소스 샘플 옮기고 DB연결설정

--- src/main/java/com/carrotglobal/sample/SampleApplication.java
+++ src/main/java/com/carrotglobal/sample/SampleApplication.java
@@ -2,8 +2,10 @@ package com.carrotglobal.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages= {"com.carrotglobal"}) // basePackage를 정해줘서 어노테이션 스캔하도록 설정
 public class SampleApplication {
 // 주석
 	public static void main(String[] args) {
